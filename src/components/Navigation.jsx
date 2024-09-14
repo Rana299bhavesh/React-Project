55e9414bfd3237e9f4f40a33fdf01dcbd0f88e78
Rana bhavesh <ranabhavesh299@gmail.com>
2024-09-14 18:44:39 +0530

    added

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,21 @@
+const Navigation = ()=>{
+    return(
+        <nav className="conatiner">
+                <div className="logo">
+                    <img src="\public\images\brand_logo (4).png" alt="logo" />
+
+                </div>
+
+                <ul>
+                    <li href="#">Menu</li>
+                    <li href="#">Location</li>
+                    <li href="#">About</li>
+                    <li href="#">Contact</li>
+                </ul>
+                <button>login</button>
+            </nav>
+
+    )
+};
+
+export default Navigation; 
